5edf1fc23df19b3be028e686630aee5586d59c12
Raymond Camden <raymondcamden@gmail.com>
2019-11-24 12:23:38 -0600

    oops

--- Chapter09/Example9.06/mapgetters/src/App.vue
+++ Chapter09/Example9.06/mapgetters/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <p>
-    My name is {{ name }}
+    My name is {{ firstName }} {{ lastName}}, or just {{ name }}.
     </p>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import { mapState } from 'vuex';
 
 export default {
   name: 'app',
   computed: {
+    ...mapState([ "firstName", "lastName" ]),
     ...mapGetters([
       "name"
     ])
