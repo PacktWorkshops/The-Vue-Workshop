5609f63698ed15d4321c245ec27e29d85d8d2b88
Maya Shavin <maya@cloudinary.com>
2019-12-26 10:55:21 +0200

    Add exercises to chapter 7

--- Chapter07/exercise7.01/src/App.vue
+++ Chapter07/exercise7.01/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <message-editor/>    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MessageEditor from './components/MessageEditor.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MessageEditor
   }
 }
 </script>
--- Chapter07/exercise7.02/src/App.vue
+++ Chapter07/exercise7.02/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <messages/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Messages from './components/Messages.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Messages
   }
 }
 </script>
