fd8775c23647612817b5360ff1248babb02eed21
Raymond Camden <raymondcamden@gmail.com>
2020-06-16 10:26:16 -0500

    fix

--- Chapter11/exercise11.01/src/store/actions.js
+++ Chapter11/exercise11.01/src/store/actions.js
@@ -1,7 +1,7 @@
 export default {
 	async updateFavoriteFilm(context, film) {
 		try {
-			let response = await fetch('https://swapi.co/api/films?search='+encodeURIComponent(film));
+			let response = await fetch('https://swapi.dev/api/films?search='+encodeURIComponent(film));
 			let data = await response.json();
 			if(data.count === 1) context.commit('setFavoriteFilm', film);
 			else console.log('Ignored setting non-Star Wars film '+film+' as favorite.'); 
